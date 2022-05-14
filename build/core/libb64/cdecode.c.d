9991c9ca0d1e1a5f07f9993157f35f3882323b55
Jonathan Chitiva <jonathanchitivam@gmail.com>
2022-05-14 18:19:14 -0500

    Eliminacion archivos inutiles

--- build/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-d:\Documentos\ProyectoGrado\build\core\libb64\cdecode.c.o: \
- C:\Users\xGandax\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cdecode.c \
- C:\Users\xGandax\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6\cores\esp32\libb64\cdecode.h \
- C:\Users\xGandax\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/stdint.h \
- C:\Users\xGandax\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/machine/_default_types.h \
- C:\Users\xGandax\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/sys/features.h \
- C:\Users\xGandax\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.6/tools/sdk/include/newlib/sys/_intsup.h
