e17bb9ce51a0bd6d8e5133611fe1d22fee6d54a4
Jonathan Chitiva <jonathanchitivam@gmail.com>
2022-06-21 17:24:45 -0500

    Actualizacion Copia

--- Dupicadora/Dupicadora.ino
+++ Dupicadora/Dupicadora.ino
@@ -7,6 +7,10 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>
 
+#define LABEL1_FONT &FreeSerif12pt7b
+#define LABEL2_FONT &FreeSansOblique12pt7b
+
+
 
 #define CALIBRATION_FILE "/TouchCalData3"
 #define REPEAT_CAL false
@@ -34,7 +38,6 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
-
 TFT_eSPI_Button copia;
 TFT_eSPI_Button generar;
 TFT_eSPI_Button copiaListo;
@@ -46,18 +49,18 @@ bool key_ready = false;
 uint16_t steps_x = 0;
 uint16_t steps_a = 0;
 uint16_t llave[1500];
-uint8_t progreso = 5;
+uint16_t progreso = 0;
 uint16_t t_x = 0, t_y = 0;
+boolean pressed;
 
 hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 void IRAM_ATTR onTimer() {
   portENTER_CRITICAL_ISR(&timerMux);
-  actualizarCarga(tft, progreso);
-  if (progreso < 255) {
+  tft.fillRoundRect(30, 120, progreso, 60, 20, TFT_GREEN);
+  tft.drawRoundRect(26, 116, 268, 68, 20, TFT_WHITE);
+  if (progreso < 260) {
     progreso++;
-  } else {
-    progreso = 255;
   }
   portEXIT_CRITICAL_ISR(&timerMux);
 
@@ -69,10 +72,57 @@ Stepper Motor_a(Pin_Enable_Motor_A, Pin_Dir_Motor_A, Pin_Pulse_Motor_A, Pin_Sens
 
 void setup()
 {
+  // Inicialixacion serial (Solo pruebas)
   Serial.begin(9600);
   // Inicializacion pantalla
   tft.begin();
   tft.setRotation(1);
+
+  // Inicializacion de todos los botones
+  copia.initButton(&tft, // REF - LEAVE AS IS
+                   160, // X Cord: SEE ABOVE Line 19
+                   70, // Y CORD: SEE ABOVE Line 20
+                   250, // WIDTH: SEE ABOVE Line 21
+                   50, // HEIGHT: SEE ABOVE Line 22
+                   TFT_WHITE, // OUTLINE
+                   TFT_BLUE, // TEXT COLOR
+                   TFT_WHITE, // FILL
+                   "Copiar", // TEXT TO PRINT
+                   1); // TEXT SIZE: SEE ABOVE Line 23
+
+  generar.initButton(&tft, // REF - LEAVE AS IS
+                     160, // X Cord: SEE ABOVE Line 19
+                     150, // Y CORD: SEE ABOVE Line 20
+                     250, // WIDTH: SEE ABOVE Line 21
+                     50, // HEIGHT: SEE ABOVE Line 22
+                     TFT_WHITE, // OUTLINE
+                     TFT_BLUE, // TEXT COLOR
+                     TFT_WHITE, // FILL
+                     "Generar", // TEXT TO PRINT
+                     1); // TEXT SIZE: SEE ABOVE Line 23
+
+  copiaListo.initButton(&tft, // REF - LEAVE AS IS
+                        160, // X Cord: SEE ABOVE Line 19
+                        200, // Y CORD: SEE ABOVE Line 20
+                        150, // WIDTH: SEE ABOVE Line 21
+                        50, // HEIGHT: SEE ABOVE Line 22
+                        TFT_WHITE, // OUTLINE
+                        TFT_BLUE, // TEXT COLOR
+                        TFT_WHITE, // FILL
+                        "Listo", // TEXT TO PRINT
+                        1); // TEXT SIZE: SEE ABOVE Line 23
+
+  Home.initButton(&tft, // REF - LEAVE AS IS
+                  10, // X Cord: SEE ABOVE Line 19
+                  10, // Y CORD: SEE ABOVE Line 20
+                  20, // WIDTH: SEE ABOVE Line 21
+                  20, // HEIGHT: SEE ABOVE Line 22
+                  TFT_WHITE, // OUTLINE
+                  TFT_RED, // TEXT COLOR
+                  TFT_WHITE, // FILL
+                  "x", // TEXT TO PRINT
+                  1); // TEXT SIZE: SEE ABOVE Line 23
+
   // Inicializacion de los pines de los motores
   pinMode(Pin_Enable_Motor_X, OUTPUT);
   pinMode(Pin_Dir_Motor_X, OUTPUT);
@@ -85,51 +135,48 @@ void setup()
   pinMode(Pin_Sensor_A, INPUT_PULLDOWN);
   digitalWrite(Pin_Rele, LOW);
 
+  // Definicion del timer para la pantalla de progreso
   timer = timerBegin(0, 80, true);
   timerAttachInterrupt(timer, &onTimer, true);
   timerAlarmWrite(timer, 212000, true);
 
+  // Calibracion del tactil
   touch_calibrate();
 
   // Reset de los motores
-  Motor_a.setSpeed(50);
   Motor_a.reset();
-  Motor_x.setSpeed(25);
   Motor_x.reset();
-
-  //    Motor_a.setSpeed(200);
-  //    Motor_a.MoveSteps(5200);
-  //    Motor_x.setSpeed(50);
-  //    Motor_x.MoveSteps(30000);
   Motor_x.sound(4);
   delay(300);
   Motor_x.sound(4);
-
-  //Motor_a.disable();
-  //Motor_x.disable();
-
-  // Motor_a.disable();
-  // Motor_x.disable();
-
-  // generarLlave(llave);
-  //     Serial.println("empezando ...");
-  //     Motor_x.setSpeed(25);
-  //     Motor_x.reset();
+  Serial.println("Empezando ...");
 }
 
 void loop()
 {
-  progreso = 0;
+MenuPrincipal:
   Serial.println("Seleccione modo a utilizar:");
   Serial.println("C: Copiar");
   Serial.println("G: Generar");
-  menu(tft, copia, generar);
+
+  // Generacion del menu principal
+  tft.setFreeFont(LABEL1_FONT);
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(110, 15);
+  tft.setTextColor(TFT_BLUE);
+  tft.setTextSize(1);
+  tft.println("Menu V0.5");
+  tft.drawLine(0, 20, 320, 20, TFT_BLUE);
+  tft.setTextSize(1);
+  tft.setFreeFont(LABEL2_FONT);
+  copia.drawButton();
+  generar.drawButton();
 
   while (true)
   {
     t_x = 0;
     t_y = 0;
-    boolean pressed = tft.getTouch(&t_x, &t_y);
+    pressed = tft.getTouch(&t_x, &t_y);
     if (Serial.available() || pressed )
     {
       String str = Serial.readStringUntil('\n');
@@ -140,34 +187,62 @@ void loop()
       Serial.println(t_y);
       if (str == "c" || copia.contains(t_x, t_y))
       {
-        menuCopia(tft,copiaListo,Home);
+        //Inicializacion del modo copia
         Motor_a.reset();
         Motor_x.reset();
         Motor_x.setSpeed(25);
         Motor_x.MovePosition(31500);
-        // Pantallazo "introduzca las llaves y oprima listo"
+
+        // Generacion pantalla de copia
+        tft.setFreeFont(LABEL1_FONT);
+        tft.fillScreen(TFT_BLACK);
+        tft.setCursor(130, 15);
+        tft.setTextColor(TFT_BLUE);
+        tft.setTextSize(1);
+        tft.println("Copiar");
+        tft.drawLine(0, 20, 320, 20, TFT_BLUE);
+        tft.fillRoundRect(30, 50, 260, 110, 20, TFT_BLUE);
+        tft.drawRoundRect(26, 46, 268, 118, 20, TFT_BLUE);
+        tft.setCursor(40, 85);
+        tft.setTextColor(TFT_WHITE, TFT_BLUE);
+        tft.println("Coloque la llave a copiar");
+        tft.setCursor(35, 120);
+        tft.setTextColor(TFT_WHITE, TFT_BLUE);
+        tft.println("y el modelo en la maquina");
+        tft.setTextSize(2);
+        tft.setFreeFont(LABEL2_FONT);
+        copiaListo.drawButton();
+        tft.setFreeFont(LABEL1_FONT);
+        Home.drawButton();
         Serial.println("Oprima cualquier tecla para continuar");
-        key_ready = false;
 
         while (true)
         {
           t_x = 0;
           t_y = 0;
-          if (Serial.available()||pressed )
+          pressed = tft.getTouch(&t_x, &t_y);
+          if (Serial.available() || (pressed && copiaListo.contains(t_x, t_y)))
           {
-            key_ready = true;
-          }
-
-          if (key_ready == true)
-          {
+            // Activacion barra de progreso
             timerAlarmEnable(timer);
-            copiaCarga(tft);
+            // Generacion barra de carga
+            tft.setFreeFont(LABEL1_FONT);
+            tft.fillScreen(TFT_BLACK);
+            tft.setCursor(130, 15);
+            tft.setTextColor(TFT_BLUE);
+            tft.setTextSize(1);
+            tft.println("Copiar");
+            tft.drawLine(0, 20, 320, 20, TFT_BLUE);
+            tft.setCursor(110, 85);
+            tft.setTextColor(TFT_WHITE, TFT_BLUE);
+            tft.println("Copiando...");
+            tft.drawRoundRect(26, 116, 268, 68, 20, TFT_WHITE);
+            Serial.println("Realizando llave ...");
             break;
+          } else if (pressed && Home.contains(t_x, t_y)) {
+            goto MenuPrincipal;
           }
         }
-        // Pantallazo "espere a que termine...""
-        Serial.println("Realizando llave ...");
-
         Motor_a.setSpeed(100);
         Motor_a.MovePosition(5300);
         digitalWrite(Pin_Rele, HIGH);
@@ -187,10 +262,27 @@ void loop()
         Motor_x.sound(4);
         delay(300);
         Motor_x.sound(4);
+
+        //Deshabilita la pantalla de carga
         timerAlarmDisable(timer);
-        finalizado(tft,Home);
+        progreso = 0;
+
         //  Pantallazo "finalizado"
+        tft.setFreeFont(LABEL1_FONT);
+        tft.fillScreen(TFT_BLACK);
+        tft.setCursor(130, 15);
+        tft.setTextColor(TFT_BLUE);
+        tft.setTextSize(1);
+        tft.println("Copiar");
+        tft.drawLine(0, 20, 320, 20, TFT_BLUE);
+        tft.setCursor(110, 90);
+        tft.setTextColor(TFT_WHITE, TFT_BLUE);
+        tft.println("¡Finalizado!");
+        tft.setTextSize(2);
+        tft.setFreeFont(LABEL2_FONT);
+        Home.drawButton();
         Serial.println("Finalizado");
+
         //  Pantallazo "activar grata
         Serial.println("Desea activar grata:");
         Serial.println("S: Si");
@@ -224,7 +316,14 @@ void loop()
         }
         break;
       }
-
+      /*
+        Funciones solo para pruebas
+        m   >> apaga la fresa
+        n   >> enciende la fresa
+        r   >> hace un reset
+        x=# >> mueve el motor x a el paso #
+        a=# >> mueve el motor a a el paso #
+      */
       else if (str == "m")
       {
         digitalWrite(Pin_Rele, LOW);
@@ -233,33 +332,36 @@ void loop()
       {
         digitalWrite(Pin_Rele, HIGH);
       }
+      else if (str == "r")
+      {
+        Motor_a.reset();
+        Motor_x.reset();
+      }
+      else if (str[0] == 'x')
+      {
+        int posd = str.substring(2).toInt();
+        Motor_x.setSpeed(50);
+        Motor_x.MovePosition(posd);
+        Serial.print("x=");
+        Serial.println(posd);
+      }
+      else if (str[0] == 'a')
+      {
+        int posd = str.substring(2).toInt();
+        if (posd < 0)
+        {
+          Motor_a.disable();
+        }
+        else
+        {
+          Motor_a.setSpeed(200);
+          Motor_a.MovePosition(posd);
+          Serial.print("a=");
+          Serial.println(posd);
+        }
+      }
     }
   }
-
-  //  if (Serial.available()) {
-  //    String str = Serial.readStringUntil('\n');
-  //
-  //    Serial.println("recibido");
-  //    if (str[0] == 'x') {
-  //      int posd = str.substring(2).toInt();
-  //      Motor_x.setSpeed(50);
-  //      Motor_x.MovePosition(posd);
-  //      Serial.print("x=");
-  //      Serial.println(posd);
-  //    }
-  //    if (str[0] == 'a') {
-  //      int posd = str.substring(2).toInt();
-  //      if (posd < 0) {
-  //        Motor_a.disable();
-  //      } else {
-  //        Motor_a.setSpeed(200);
-  //        Motor_a.MovePosition(posd);
-  //        Serial.print("a=");
-  //        Serial.println(posd);
-  //      }
-  //    }
-  //  }
-  // Pantallazo "Menu principal:Copia, generar llave"
 }
 
 
@@ -315,7 +417,6 @@ void touch_calibrate()
     }
 
     tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
-
     tft.setTextColor(TFT_GREEN, TFT_BLACK);
     tft.println("Calibration complete!");
 
